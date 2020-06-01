19cf3f21163df6a9fa4f14fcbff0418685586be7
KIHA <flklc901@gmail.com>
2020-06-01 15:56:50 +0900

    partnerServiceImpl_sql_dao_정의_기하
    partnerServiceImpl_sql_dao_정의_기하

--- GoodPlace/src/main/java/com/kh/goodplace/partner/model/service/PartnerServiceImpl.java
+++ GoodPlace/src/main/java/com/kh/goodplace/partner/model/service/PartnerServiceImpl.java
@@ -1,5 +1,18 @@
 package com.kh.goodplace.partner.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.goodplace.partner.model.dao.PartnerDao;
+
+@Service("rService") 
 public class PartnerServiceImpl {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private PartnerDao pDao;
 
 }
